8f7852819937a342aea032abeef3c0f1bcaf8550
ufabc <mariocairespereira@gmail.com>
2017-09-22 09:57:13 -0300

    Exercicio 01

---
+++ lista01/exc1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+int a;
+
+printf("Digite um número: ");
+scanf("%d", &a);
+
+if(a%2 == 0) {
+	printf("O número inserido é par\n");
+} else printf("O número inserido é ímpar\n");
+
+}
+	
